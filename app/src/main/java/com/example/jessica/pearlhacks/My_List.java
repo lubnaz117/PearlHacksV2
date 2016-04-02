89406de5e06782688ac89b32880773e07757c0e8
Zubair, Lubna <lubnaz@gatech.edu>
2016-04-02 18:36:03 -0400

    added button

---
+++ app/src/main/java/com/example/jessica/pearlhacks/My_List.java
@@ -0,0 +1,14 @@
+package com.example.jessica.pearlhacks;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class My_List extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my__list);
+    }
+
+}
